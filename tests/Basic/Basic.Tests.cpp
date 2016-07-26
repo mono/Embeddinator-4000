f0d3698ff28505b04c64e2470ef00092584976e8
Joao Matos <joao@tritao.eu>
2016-07-27 00:51:52 +0100

    Added native testing code for basic tests.

--- tests/Basic/Basic.Tests.cpp
+++ tests/Basic/Basic.Tests.cpp
@@ -1,10 +1,19 @@
-﻿//using System;
-//using EmptyTest;
-//using CppSharp.Utils;
-//using NUnit.Framework;
+﻿#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 
-//[TestFixture]
-//public class EmptyTests : GeneratorTestFixture
-//{
-//}
-// 
+#include "Basic.h"
+
+TEST_CASE("BuiltinTypes", "[BuiltinTypes]") {
+    BuiltinTypes* bt = BuiltinTypes_new();
+    BuiltinTypes_ReturnsVoid(bt);
+    REQUIRE(BuiltinTypes_ReturnsBool(bt)   == true);
+    REQUIRE(BuiltinTypes_ReturnsSByte(bt)  == -5);
+    REQUIRE(BuiltinTypes_ReturnsByte(bt)   ==  5);
+    REQUIRE(BuiltinTypes_ReturnsShort(bt)  == -5);
+    REQUIRE(BuiltinTypes_ReturnsUShort(bt) ==  5);
+    REQUIRE(BuiltinTypes_ReturnsInt(bt)    == -5);
+    REQUIRE(BuiltinTypes_ReturnsUInt(bt)   ==  5);
+    REQUIRE(BuiltinTypes_ReturnsLong(bt)   == -5);
+    REQUIRE(BuiltinTypes_ReturnsULong(bt)  ==  5);
+    REQUIRE(BuiltinTypes_ReturnsString(bt) == "Mono");
+}
