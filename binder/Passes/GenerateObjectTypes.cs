336922f49c239d2e2a3721406f294311e1b0c131
Joao Matos <joao@tritao.eu>
2016-10-24 15:43:40 +0100

    Fixed GenerateObjectTypes pass to keep track of explicit classes not in tag types.

--- binder/Passes/GenerateObjectTypes.cs
+++ binder/Passes/GenerateObjectTypes.cs
@@ -8,12 +8,15 @@ namespace MonoManagedToNative.Passes
     {
         TranslationUnit TranslationUnit;
 
-        List<TypedefDecl> Declarations;
+        public List<TypedefDecl> Declarations;
+        public List<Class> Classes;
+
         HashSet<Declaration> Tags;
 
         public GenerateObjectTypesPass()
         {
             Declarations = new List<TypedefDecl>();
+            Classes = new List<Class>();
             Tags = new HashSet<Declaration>();
         }
 
@@ -26,22 +29,19 @@ namespace MonoManagedToNative.Passes
             unit.Declarations.InsertRange(0, Declarations);
 
             Declarations.Clear();
+            Classes.Clear();
             Tags.Clear();
             TranslationUnit = null;
 
             return ret;
         }
 
-        public override bool VisitTagType(TagType tag, TypeQualifiers quals)
+        bool HandleClass(Class @class)
         {
-            var @class = tag.Declaration as Class;
-
-            if (@class == null)
-                return false;
-
             if (Tags.Contains(@class))
                 return false;
 
+            Classes.Add(@class);
             Tags.Add(@class);
 
             var monoObjectType = new Class { Name = "MonoEmbedObject" };
@@ -54,7 +54,24 @@ namespace MonoManagedToNative.Passes
             };
 
             Declarations.Add(typedef);
+
             return true;
+        }
+
+        public override bool VisitClassDecl(Class @class)
+        {
+            HandleClass(@class);
+            return base.VisitClassDecl(@class);
+        }
+
+        public override bool VisitTagType(TagType tag, TypeQualifiers quals)
+        {
+            var @class = tag.Declaration as Class;
+
+            if (@class == null)
+                return false;
+
+            return HandleClass(@class);
         }
     }
 }
