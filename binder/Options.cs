1ee2645e6150eb9044b1e7f38d51e574882af374
Joao Matos <joao@tritao.eu>
2017-02-01 19:31:35 +0000

    Bump to latest CppSharp and remove more now unused options.

--- binder/Options.cs
+++ binder/Options.cs
@@ -1,50 +1,14 @@
 ﻿using CppSharp;
-using CppSharp.Generators;
 
 namespace MonoEmbeddinator4000
 {
-    public enum CompilationTarget
-    {
-        SharedLibrary,
-        StaticLibrary,
-        Application
-    }
-
     public class Options : DriverOptions
     {
         public Options()
         {
             Project = new Project();
-            GenerateSupportFiles = true;
         }
 
         public Project Project;
-
-        // Generator options
-        public TargetPlatform Platform;
-
-        /// <summary>
-        /// Specifies the VS version.
-        /// </summary>
-        /// <remarks>When null, latest is used.</remarks>
-        public VisualStudioVersion VsVersion;
-
-        // If code compilation is enabled, then sets the compilation target.
-        public CompilationTarget Target;
-
-        // If true, will force the generation of debug metadata for the native
-        // and managed code.
-        public bool DebugMode;
-
-        // If true, will use unmanaged->managed thunks to call managed methods.
-        // In this mode the JIT will generate specialized wrappers for marshaling
-        // which will be faster but also lead to higher memory consumption.
-        public bool UseUnmanagedThunks;
-
-        // If true, will generate support files alongside generated binding code.
-        public bool GenerateSupportFiles;
-
-        // If true, will compile the generated as a shared library / DLL.
-        public bool CompileSharedLibrary => Target == CompilationTarget.SharedLibrary;
     }
 }
