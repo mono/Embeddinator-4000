21b636b67da59ac635585e910f1566df8a667024
Joao Matos <joao@tritao.eu>
2017-03-06 16:40:40 +0000

    Added initial generation of method parameters.

--- binder/Generators/Java/JavaSources.cs
+++ binder/Generators/Java/JavaSources.cs
@@ -172,6 +172,15 @@ namespace MonoEmbeddinator4000.Generators
             return true;
         }
 
+        private string FormatMethodParameters(IEnumerable<Parameter> @params)
+        {
+            return string.Join(", ",
+                from param in @params
+                where param.Kind != ParameterKind.IndirectReturnType && !param.Ignore
+                let typeName = param.CSharpType(TypePrinter)
+                select string.Format("{0} {1}", typeName, param.Name));
+        }
+
         public override void GenerateMethodSpecifier(Method method, Class @class)
         {
             var keywords = new List<string>();
@@ -198,7 +207,7 @@ namespace MonoEmbeddinator4000.Generators
             else
                 Write("{0} {1}(", method.ReturnType, functionName);
 
-            //Write(FormatMethodParameters(method.Parameters));
+            Write(FormatMethodParameters(method.Parameters));
 
             Write(")");
         }
