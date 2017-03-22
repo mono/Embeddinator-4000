dc32c9ad52560beeea3b987a6d388819637c3172
Joao Matos <joao@tritao.eu>
2017-03-22 15:21:57 +0000

    Added target= CLI option to specify compilation target.

--- binder/CLI.cs
+++ binder/CLI.cs
@@ -33,6 +33,7 @@ namespace MonoEmbeddinator4000
                 { "o|out|outdir=", "output directory", v => OutputDir = v },
                 { "c|compile", "compiles the generated output", v => CompileCode = true },
                 { "d|debug", "enables debug mode for generated native and managed code", v => DebugMode = true },
+                { "t|target", "compilation target (static, shared, app)", v => Target = ConvertToCompilationTarget(v) },
                 { "dll|shared", "compiles as a shared library", v => Target = CompilationTarget.SharedLibrary },
                 { "static", "compiles as a static library", v => Target = CompilationTarget.StaticLibrary },
                 { "vs=", $"Visual Studio version for compilation: {vsVersions} (defaults to Latest)", v => VsVersion = v },
@@ -68,6 +69,22 @@ namespace MonoEmbeddinator4000
                 Console.WriteLine("At least one managed assembly must be passed as input.");
                 Environment.Exit(0);
             }
+        }
+
+        static CompilationTarget ConvertToCompilationTarget(string target)
+        {
+            switch(target.ToLowerInvariant())
+            {
+            case "static":
+                return CompilationTarget.StaticLibrary;
+            case "shared":
+                return CompilationTarget.SharedLibrary;
+            case "app":
+            case "exe":
+                return CompilationTarget.Application;
+            }
+
+            throw new NotSupportedException("Unknown compilation target: " + target);
         }
 
         static GeneratorKind ConvertToGeneratorKind(string gen)
