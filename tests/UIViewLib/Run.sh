94be797c92195f65a76c99d857d2e3445b02deef
Joao Matos <joao@tritao.eu>
2017-03-23 17:26:48 +0000

    Minor correctness fix to UIViewLib sample run script.

--- tests/UIViewLib/Run.sh
+++ tests/UIViewLib/Run.sh
@@ -1,3 +1,3 @@
 CUR_DIR=$(dirname -- $0)
 msbuild /nologo /v:minimal $CUR_DIR/UIViewLib.sln
-mono --debug ../../build/lib/Debug/MonoEmbeddinator4000.exe --gen=objc -compile -p=macos -target=shared --out=mac $CUR_DIR/bin/Debug/UIViewLib.dll
+mono --debug $CUR_DIR/../../build/lib/Debug/MonoEmbeddinator4000.exe --gen=objc -compile -p=macos -target=shared --out=uiviewlib_mac $CUR_DIR/bin/Debug/UIViewLib.dll
