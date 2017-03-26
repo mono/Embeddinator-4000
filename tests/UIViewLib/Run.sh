40208cf25b8fff7789f14b162a4d39858e60984b
Joao Matos <joao@tritao.eu>
2017-03-27 00:30:33 +0100

    Improve the sample run script to be more extensible and generate for more target languages.

--- tests/UIViewLib/Run.sh
+++ tests/UIViewLib/Run.sh
@@ -1,4 +1,31 @@
 CUR_DIR=$(dirname -- $0)
-msbuild /nologo /v:minimal $CUR_DIR/UIViewLib.sln
-mono --debug $CUR_DIR/../../build/lib/Debug/MonoEmbeddinator4000.exe --gen=objc -compile -p=macos -target=shared --out=uiviewlib_objc $CUR_DIR/bin/Debug/UIViewLib.dll
-mono --debug $CUR_DIR/../../build/lib/Debug/MonoEmbeddinator4000.exe --gen=c -compile -p=macos -target=shared --out=uiviewlib_c $CUR_DIR/bin/Debug/UIViewLib.dll
+EMBEDDINATOR=$CUR_DIR/../../build/lib/Debug/MonoEmbeddinator4000.exe
+
+PLATFORM=macos
+TARGET=shared
+CONFIGURATION=Debug
+
+NAME=UIViewLib
+SOLUTION=$CUR_DIR/$NAME.sln
+ASSEMBLY=$CUR_DIR/bin/$CONFIGURATION/$NAME.dll
+
+while [[ "$#" > 1 ]]; do case $1 in
+    --verbose) VERBOSE=true;;
+    --v) verbose=true;;
+    *) break;;
+  esac; shift; shift
+done
+
+FLAGS=
+VERBOSE=false
+
+if [ "$VERBOSE" = true ]; then
+	FLAGS+=-verbose
+fi
+
+msbuild /nologo /v:minimal $SOLUTION
+
+for LANG in c objc java
+do
+	mono --debug $EMBEDDINATOR --gen=$LANG -compile -p=$PLATFORM -target=$TARGET $FLAGS --out=$CUR_DIR/gen/$LANG $ASSEMBLY
+done
