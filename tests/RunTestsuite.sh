80eb0313a86f71d4e5fa27cf370fa25200d251ca
Joao Matos <joao@tritao.eu>
2017-03-31 17:36:39 +0100

    [tests] Fallback on xbuild if msbuild is not available on the system.

--- tests/RunTestsuite.sh
+++ tests/RunTestsuite.sh
@@ -1,3 +1,3 @@
-msbuild /nologo /v:minimal ../build/MonoEmbeddinator4000.sln
+msbuild /nologo /v:minimal ../build/MonoEmbeddinator4000.sln || xbuild /nologo /v:minimal ../build/MonoEmbeddinator4000.sln
 make
 ./Basic/Basic.Tests
