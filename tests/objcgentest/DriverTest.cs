4f3f87d82bc7774b6777c8053fdaad850dff0e0d
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-05 14:13:37 -0500

    [objc][driver] Command-line options parity with master
    This will help scripts to switch between implementations.

--- tests/objcgentest/DriverTest.cs
+++ tests/objcgentest/DriverTest.cs
@@ -28,5 +28,99 @@ namespace DriverTest {
 				Directory.Delete (valid);
 			}
 		}
+
+		[Test]
+		public void Target_Supported ()
+		{
+			var valid = new string [] { "ObjC", "Obj-C", "ObjectiveC", "objective-c" };
+			foreach (var t in valid) {
+				Driver.Target = t;
+				Assert.That (Driver.Target, Is.EqualTo ("objc"), t);
+			}
+		}
+
+		[Test]
+		public void Target_NotSupported ()
+		{
+			var notsupported = new string [] { "C", "C++", "Java" };
+			foreach (var t in notsupported) {
+				try {
+					Driver.Target = t;
+				}
+				catch (EmbeddinatorException ee) {
+					Assert.True (ee.Error, "Error");
+					Assert.That (ee.Code, Is.EqualTo (4), "Code");
+				}
+			}
+		}
+
+		[Test]
+		public void Target_Invalid ()
+		{
+			try {
+				Driver.Target = "invalid";
+			}
+			catch (EmbeddinatorException ee) {
+				Assert.True (ee.Error, "Error");
+				Assert.That (ee.Code, Is.EqualTo (4), "Code");
+			}
+		}
+
+		[Test]
+		public void Platform_Supported ()
+		{
+			var valid = new string [] { "OSX", "MacOSX", "MacOS", "Mac" };
+			foreach (var p in valid) {
+				Driver.Platform = p;
+				Assert.That (Driver.Platform, Is.EqualTo ("macos"), p);
+			}
+		}
+
+		[Test]
+		public void Platform_NotSupported ()
+		{
+			var notsupported = new string [] { "Windows", "Android", "iOS", "tvOS", "watchOS" };
+			foreach (var p in notsupported) {
+				try {
+					Driver.Platform = p;
+				} catch (EmbeddinatorException ee) {
+					Assert.True (ee.Error, "Error");
+					Assert.That (ee.Code, Is.EqualTo (3), "Code");
+				}
+			}
+		}
+
+		[Test]
+		public void Platform_Invalid ()
+		{
+			try {
+				Driver.Platform = "invalid";
+			} catch (EmbeddinatorException ee) {
+				Assert.True (ee.Error, "Error");
+				Assert.That (ee.Code, Is.EqualTo (3), "Code");
+			}
+		}
+
+		[Test]
+		public void Static_Unsupported ()
+		{
+			try {
+				Driver.Main2 (new [] { "--static" });
+			} catch (EmbeddinatorException ee) {
+				Assert.True (ee.Error, "Error");
+				Assert.That (ee.Code, Is.EqualTo (2), "Code");
+			}
+		}
+
+		[Test]
+		public void VS_Unsupported ()
+		{
+			try {
+				Driver.Main2 (new [] { "--vs=x" });
+			} catch (EmbeddinatorException ee) {
+				Assert.True (ee.Error, "Error");
+				Assert.That (ee.Code, Is.EqualTo (2), "Code");
+			}
+		}
 	}
 }
