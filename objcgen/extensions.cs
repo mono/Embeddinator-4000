4fdd4a2892ddacc0dd99da69560b16f52556ada8
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-13 18:02:41 -0400

    [objc] Add support for enums and [Flags]

---
+++ objcgen/extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using IKVM.Reflection;
+using Type = IKVM.Reflection.Type;
+
+namespace Embeddinator {
+
+	public static class TypeExtensions {
+		
+		public static bool Is (this Type self, string @namespace, string name)
+		{
+			return (self.Namespace == @namespace) && (self.Name == name);
+		}
+
+		public static bool HasCustomAttribute (this Type self, string @namespace, string name)
+		{
+			foreach (var ca in self.CustomAttributes) {
+				if (ca.AttributeType.Is (@namespace, name))
+					return true;
+			}
+			return false;
+		}
+	}
+}
