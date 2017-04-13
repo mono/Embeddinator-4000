f3ff1c95b6c21888f9e0cdc955ca57f37b299306
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-13 15:34:46 -0400

    [objc] Add support for structs / valuetype

---
+++ tests/managed/structs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Structs {
+
+	public struct Point {
+
+		public Point (float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public float X { get; private set; }
+
+		public float Y { get; private set; }
+
+		public static bool operator == (Point left, Point right)
+		{
+			return ((left.X == right.X) && (left.Y == right.Y));
+		}
+
+		public static bool operator != (Point left, Point right)
+		{
+			return !(left == right);
+		}
+	}
+}
