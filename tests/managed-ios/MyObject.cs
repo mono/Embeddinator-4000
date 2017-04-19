6f80c460dbc3e4fe12c730501b182bc6f12d689c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-04-19 13:03:27 +0200

    [tests][objc] Add tests for NSObject subclasses.

---
+++ tests/managed-ios/MyObject.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+
+namespace CustomUI
+{
+    public class MyObject : NSObject
+    {
+		[Export ("addStatic:to:")]
+		public static int AddStatic (int a, int b)
+		{
+			return a + b;
+		}
+
+		[Export ("addInstance:to:")]
+		public int AddInstance (int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
