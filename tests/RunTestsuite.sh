3a563e80eccc859b5a10c50bbf1c8fda65f258b5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-04-21 16:56:27 +0200

    [objc] Disable the C tests according to conversation with João.

--- tests/RunTestsuite.sh
+++ tests/RunTestsuite.sh
@@ -1,3 +1,5 @@
+echo Disabled
+exit 0
 CUR_DIR=$(dirname -- $0)
 msbuild /nologo /v:minimal $CUR_DIR/../build/MonoEmbeddinator4000.sln || xbuild /nologo /v:minimal $CUR_DIR/../build/MonoEmbeddinator4000.sln
 make -C $CUR_DIR
