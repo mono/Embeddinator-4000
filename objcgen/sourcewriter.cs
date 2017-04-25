8e9bff0ae9e398d0a6ea82670a81977eaf6d18e8
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-24 20:06:43 -0400

    [objc] Add a new SourceWriter to replace the TextWriter
    * Allow enabling/disabling the writer (needed for other features like
      protocols);
    
    * Deal with tabs in a single place - so output looks better;

---
+++ objcgen/sourcewriter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Embeddinator {
+	
+	public class SourceWriter : TextWriter {
+
+		StringBuilder sb = new StringBuilder ();
+		bool newline = true;
+
+		public bool Enabled { get; set; } = true;
+		public int Indent { get; set; }
+
+		public override Encoding Encoding => Encoding.UTF8;
+
+		void ApplyTabs ()
+		{
+			if (newline) {
+				for (int i = 0; i < Indent; i++)
+					sb.Append ('\t');
+				newline = false;
+			}
+		}
+
+		// minimum to override - see http://msdn.microsoft.com/en-us/library/system.io.textwriter.aspx
+		public override void Write (char value)
+		{
+			if (!Enabled)
+				return;
+			ApplyTabs ();
+			sb.Append (value);
+		}
+
+		public override void WriteLine ()
+		{
+			if (!Enabled)
+				return;
+			sb.AppendLine ();
+			newline = true;
+		}
+
+		public override void Write (string value)
+		{
+			if (!Enabled)
+				return;
+			ApplyTabs ();
+			sb.Append (value);
+		}
+
+		public override string ToString ()
+		{
+			return sb.ToString ();
+		}
+	}
+}
