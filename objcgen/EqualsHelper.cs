a656e677e4d8c989d4cd20ee6d109471e65d41db
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-26 20:38:02 -0400

    [objc] Simplify EqualsHelper by calling WriteInvoke (#192)
    This will pick up the virtual call fix once PR #191 lands.

--- objcgen/EqualsHelper.cs
+++ objcgen/EqualsHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 using Embeddinator;
 
@@ -21,28 +20,11 @@ namespace ObjC {
 			implementation.Indent--;
 			WriteMethodLookup ();
 
-			implementation.WriteLine ("MonoObject* __exception = nil;");
-			var instance = "nil";
-
-			implementation.WriteLine ($"MonoObject* __instance = mono_gchandle_get_target (_object->_handle);");
-			instance = "__instance";
-
 			implementation.WriteLine ("void* __args [1];");
 			implementation.WriteLine ("int gchandle = [other xamarinGetGCHandle];");
 			implementation.WriteLine ("__args[0] = mono_gchandle_get_target (gchandle);");
 
-			implementation.Write ("MonoObject* __result = ");
-			implementation.WriteLine ($"mono_runtime_invoke (__method, {instance}, __args, &__exception);");
-
-			implementation.WriteLine ("if (__exception)");
-			implementation.Indent++;
-			if (IgnoreException) {
-				// TODO: Apple often do NSLog (or asserts but they are more brutal) and returning nil is allowed (and common)
-				implementation.WriteLine ("return nil;");
-			} else {
-				implementation.WriteLine ("mono_embeddinator_throw_exception (__exception);");
-			}
-			implementation.Indent--;
+			WriteInvoke ("__args");
 
 			implementation.WriteLine ("void* __unbox = mono_object_unbox (__result);");
 			implementation.WriteLine ("return *((bool*)__unbox);");
