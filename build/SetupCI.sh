85d694c98bf111a694c6197864845dfddcc3f22b
Joao Matos <joao@tritao.eu>
2017-05-05 00:27:21 +0100

    [travis] Setup Mono.TextTransform as workaround for T4 template build failure.

---
+++ build/SetupCI.sh
@@ -0,0 +1,11 @@
+BUILD_DIR=$(dirname -- $0)
+
+if [ ! -f $BUILD_DIR/nuget.exe ]; then
+	wget https://nuget.org/nuget.exe -O$BUILD_DIR/nuget.exe
+fi
+
+mono $BUILD_DIR/nuget.exe install Mono.TextTransform -OutputDirectory $BUILD_DIR/../deps
+
+TEXT_TEMPLATE_DIR="/Applications/Xamarin Studio.app/Contents/Resources/lib/monodevelop/AddIns/MonoDevelop.TextTemplating2/"
+mkdir -p "$TEXT_TEMPLATE_DIR"
+cp -R $BUILD_DIR/../deps/Mono.TextTransform.1.0.0/tools "$TEXT_TEMPLATE_DIR"
