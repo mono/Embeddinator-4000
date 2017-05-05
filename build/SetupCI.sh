75d7e1c4d24fa99e2ed58816eeb1d745c10e8350
Joao Matos <joao@tritao.eu>
2017-05-05 11:32:41 +0100

    [travis] Provision Mono and Xamarin.iOS using the provisioning scripts from iOS team.

--- build/SetupCI.sh
+++ build/SetupCI.sh
@@ -1,7 +1,18 @@
-BUILD_DIR=$(dirname -- $0)
+BUILD_DIR=$TRAVIS_BUILD_DIR/build
 
+cd $BUILD_DIR/../objcgen
+./system-dependencies.sh --provision
+
+OS=$(uname -s)
+if [ "$OS" == "Darwin" ]; then
+	export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin
+fi
+
+cd $BUILD_DIR
 if [ ! -f $BUILD_DIR/nuget.exe ]; then
 	wget https://nuget.org/nuget.exe -O$BUILD_DIR/nuget.exe
 fi
 
 mono $BUILD_DIR/nuget.exe install Mono.TextTransform -OutputDirectory $BUILD_DIR/../deps
+
+cd $BUILD_DIR/..
