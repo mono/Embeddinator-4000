d699210a604e1e6312039944e4b8d7fd1efa3697
Joao Matos <joao@tritao.eu>
2017-06-22 16:03:31 +0100

    [ci] Setup the PATH env variable on OSX before running the test suite.

--- tests/RunTestsuite.sh
+++ tests/RunTestsuite.sh
@@ -1,2 +1,7 @@
+OS=$(uname -s)
+if [ "$OS" == "Darwin" ]; then
+    export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin
+fi
+
 CUR_DIR=$(dirname -- $0)
 make -C $CUR_DIR/common
