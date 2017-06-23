5cebe0d962e69800bb0f35e74b6ab3e25da08a35
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-06-23 15:37:08 -0500

    [android] skip code generation for Mono.Android
    - We need to skip code generation for C# class subclassing anything in
    Mono.Android.dll
    - Next step will be to generate the Java source from Xamarin.Android
    via the `GenerateJavaStubs` MSBuild task

--- binder/Generators/AstGenerator.cs
+++ binder/Generators/AstGenerator.cs
@@ -110,7 +110,7 @@ namespace MonoEmbeddinator4000.Generators
             if (decl.Namespace == null)
                 throw new Exception("Declaration should have a namespace");
 
-            if (typeInfo.IsGenericParameter || typeInfo.IsGenericType)
+            if (typeInfo.IsGenericParameter || typeInfo.IsGenericType || typeInfo.IsAndroidSubclass())
                 decl.GenerationKind = GenerationKind.None;
 
             return decl;
@@ -124,7 +124,7 @@ namespace MonoEmbeddinator4000.Generators
 
         public void HandleBaseType(IKVM.Reflection.Type type, Class @class)
         {
-            if (type.FullName == "System.Object" || type.FullName == "System.ValueType")
+            if (type.FullName == "System.Object" || type.FullName == "System.ValueType" || type.IsAndroidSubclass())
                 return;
 
             var baseClass = Visit(type.GetTypeInfo()) as Class;
@@ -709,6 +709,20 @@ namespace MonoEmbeddinator4000.Generators
                     return true;
             }
             return false;
+        }
+
+        public static bool IsAndroidSubclass (this IKVM.Reflection.Type type)
+        {
+            do
+            {
+                if (type == null)
+                    return false;
+                if (type.Assembly.FullName.StartsWith("Mono.Android, ", StringComparison.Ordinal))
+                    return true;
+
+                type = type.BaseType;
+
+            } while (true);
         }
     }
 }
