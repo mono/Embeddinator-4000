9c3eacc710a356a59760bf291c8576a3b7278ac9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-07-05 16:23:59 -0500

    [tests] for ResourceDesignerGenerator
    Still not quite happy with the design here, more things are needed to
    clean up

---
+++ tests/MonoEmbeddinator4000Tests/Helpers/Samples.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using IKVM.Reflection;
+using Microsoft.CSharp;
+using Xamarin.Android.Tools;
+
+namespace MonoEmbeddinator4000.Tests
+{
+    public static class Samples
+    {
+        public static string MonoDroidPath { get; private set; }
+
+        static Samples()
+        {
+            AndroidLogger.Info += (task, message) => Console.WriteLine(task + ", " + message);
+            AndroidLogger.Warning += (task, message) => throw new Exception(message);
+            AndroidLogger.Error += (task, message) => throw new Exception(message);
+            AndroidSdk.Refresh();
+            MonoDroidPath = Path.GetFullPath(Path.Combine(MonoDroidSdk.BinPath, ".."));
+        }
+
+        public static Assembly LoadFile(string resourceFile)
+        {
+            var csc = new CSharpCodeProvider();
+            var temp = Path.GetTempFileName();
+
+            string source;
+            using (var stream = typeof(Samples).Assembly.GetManifestResourceStream($"MonoEmbeddinator4000.Tests.ResourceDesigner.{resourceFile}.cs"))
+            using (var reader = new StreamReader(stream))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            var parameters = new CompilerParameters
+            {
+                OutputAssembly = temp,
+            };
+            parameters.ReferencedAssemblies.Add(Path.Combine(MonoDroidPath, "lib", "xbuild-frameworks", "MonoAndroid", "v1.0", "System.dll"));
+            parameters.ReferencedAssemblies.Add(Path.Combine(MonoDroidPath, "lib", "xbuild-frameworks", "MonoAndroid", XamarinAndroidBuild.TargetFrameworkVersion, "Mono.Android.dll"));
+
+            var results = csc.CompileAssemblyFromSource(parameters, source);
+
+            if (results.Errors.HasErrors)
+            {
+                throw new Exception(results.Errors[0].ToString());
+            }
+
+            var u = new Universe();
+            foreach (var reference in parameters.ReferencedAssemblies)
+            {
+                u.LoadFile(reference);
+            }
+            return u.LoadFile(temp);
+        }
+    }
+}
---
+++ tests/MonoEmbeddinator4000Tests/ResourceDesignerTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace MonoEmbeddinator4000.Tests
+{
+    [TestFixture]
+    public class ResourceDesignerTest
+    {
+        ResourceDesignerGenerator generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            generator = new ResourceDesignerGenerator
+            {
+                MonoDroidPath = Samples.MonoDroidPath,
+                OutputDirectory = Environment.CurrentDirectory,
+                PackageName = "com.mono.embeddinator",
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            try
+            {
+                //Temp file
+                File.Delete(generator.MainAssembly);
+            }
+            catch { }
+        }
+
+        [Test]
+        public void String()
+        {
+            var assembly = Samples.LoadFile("String");
+            generator.Assemblies = new[] { assembly };
+            generator.MainAssembly = assembly.Location;
+            generator.Generate();
+
+            string source = generator.ToSource();
+            Assert.AreNotEqual("", source);
+        }
+
+        [Test]
+        public void Full()
+        {
+            var assembly = Samples.LoadFile("Full");
+            generator.Assemblies = new[] { assembly };
+            generator.MainAssembly = assembly.Location;
+            generator.Generate();
+
+            string source = generator.ToSource();
+            Assert.AreNotEqual("", source);
+        }
+    }
+}
