fc45644ab82d39378de2c01354ea942de6b6f831
Joao Matos <joao@tritao.eu>
2017-08-16 18:39:50 +0100

    [build] Remove deprecated compile and testsuite shell scripts.
    Use Cake instead!

--- tests/RunTestsuite.sh
+++
@@ -1,7 +0,0 @@
-OS=$(uname -s)
-if [ "$OS" == "Darwin" ]; then
-    export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin
-fi
-
-CUR_DIR=$(dirname -- $0)
-make -C $CUR_DIR/common
