f3a747b14afa90e082d3d49e32a7b3e51791a22e
Joao Matos <joao@tritao.eu>
2017-11-07 14:46:31 +0000

    [binder] Remove unused MarshalPrinter members.

--- binder/Generators/Marshal.cs
+++ binder/Generators/Marshal.cs
@@ -12,18 +12,13 @@ namespace Embeddinator.Generators
             Before = new TextGenerator();
             After = new TextGenerator ();
             Return = new TextGenerator();
-            MarshalVarPrefix = string.Empty;
         }
 
         public BindingContext Context { get; private set; }
 
-        public MarshalPrinter<MarshalContext> MarshalToNative;
-
         public TextGenerator Before { get; private set; }
         public TextGenerator After { get; private set; }
         public TextGenerator Return { get; private set; }
-
-        public Declaration Declaration { get; set; }
 
         public string ReturnVarName { get; set; }
         public QualifiedType ReturnType { get; set; }
@@ -31,9 +26,6 @@ namespace Embeddinator.Generators
         public string ArgName { get; set; }
         public Parameter Parameter { get; set; }
         public int ParameterIndex { get; set; }
-        public Function Function { get; set; }
-
-        public string MarshalVarPrefix { get; set; }
     }
 }
 
