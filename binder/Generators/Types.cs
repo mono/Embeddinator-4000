9c88a322f02d81af5d56702390b65f00d8061d54
Joao Matos <joao@tritao.eu>
2018-01-26 14:21:02 +0000

    [types] Extract C# type printing code in CSharpTypePrinter.

--- binder/Generators/Types.cs
+++ binder/Generators/Types.cs
@@ -13,4 +13,49 @@ namespace Embeddinator.Generators
             Original = new QualifiedType(typedef);
         }
     }
+
+    public class CSharpTypePrinter : CppTypePrinter
+    {
+        public override string VisitUnsupportedType(UnsupportedType type, TypeQualifiers quals)
+        {
+            return type.Description;
+        }
+
+        public override string VisitPrimitiveType(PrimitiveType primitive)
+        {
+            switch (primitive)
+            {
+                case PrimitiveType.Bool:
+                    return "Bool";
+                case PrimitiveType.Char:
+                    return "Char";
+                case PrimitiveType.SChar:
+                    return "SByte";
+                case PrimitiveType.UChar:
+                    return "Byte";
+                case PrimitiveType.Short:
+                    return "Int16";
+                case PrimitiveType.UShort:
+                    return "UInt16";
+                case PrimitiveType.Int:
+                    return "Int32";
+                case PrimitiveType.UInt:
+                    return "UInt32";
+                case PrimitiveType.Long:
+                    return "Int64";
+                case PrimitiveType.ULong:
+                    return "UInt64";
+                case PrimitiveType.Float:
+                    return "Single";
+                case PrimitiveType.Double:
+                    return "Double";
+                case PrimitiveType.String:
+                    return "String";
+                case PrimitiveType.Decimal:
+                    return "Decimal";
+            }
+
+            return base.VisitPrimitiveType(primitive);
+        }
+    }
 }
